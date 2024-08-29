394b4937d6cb6f4729e5627973ed1fce548e411b
rontjn <107711537+rontjn@users.noreply.github.com>
2024-08-29 22:17:42 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,7 +15,15 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+control_unit control_unit(
+    .clk(clk),
+    .reset(rst_n),
+    .start_conversion(ui_in[7]), // Signal to start ADC conversion
+    .miso(ui_in[6]), // SPI data input (from ADC)
+    .cs_n(uo_out[7]),            // Chip select for ADC
+    .sck(uo_out[6]),             // SPI clock
+    .comparison_result(uo_out[5]), // Final comparison result
+);
  
   
 
